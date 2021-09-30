afe99476956eb60e0937a6b63271c5a818279d89
Sebastien Anty <bibianty@gmail.com>
2021-09-30 16:37:49 -0400

    mongo database using node

---
+++ index.js
@@ -0,0 +1,44 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config()
+let _client;
+
+// only evre create 1 client, if it already exists just return it;
+const createClient = async () => {
+ if (!_client) {
+    _client = new MongoClient(process.env.MONGO_URL);
+    await _client.connect();
+  }
+    return _client;
+};
+
+const getUserCollection = async () => {
+    const client = await createClient();
+    const db = client.db("db2");
+    return db.collection("user");
+};
+
+const createUser = async ({ name, dob, email}) => {
+    const userCollection = await getUserCollection();
+    await userCollection.insertOne({ name, dob, email });
+    return { name, dob, email };
+};
+
+
+
+const run = async () => {
+    const client = await createClient();
+    await createUser({
+        name: "Sebas",
+        dob: new Date("02/10/2000"),
+        email: "sebas@gmail.com",
+    });
+    await client.close();
+};
+
+run().then();
+
+
+
+// createClient().then();
---
+++ index2.js
@@ -0,0 +1,90 @@
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import faker from "faker";
+
+dotenv.config();
+let _client;
+
+// only evre create 1 client, if it already exists just return it;
+const createClient = async () => {
+  if (!_client) {
+    _client = new MongoClient(process.env.MONGO_URL);
+    await _client.connect();
+  }
+  return _client;
+};
+
+const getCarsCollection = async () => {
+  const dealer = await createClient();
+  const db = dealer.db("dealership");
+  return db.collection("cars");
+};
+
+const getBuyersCollection = async () => {
+  const dealer = await createClient();
+  const db = dealer.db("dealership");
+  return db.collection("cars");
+};
+
+const getOrdersCollection = async () => {
+  const dealer = await createClient();
+  const db = dealer.db("dealership");
+  return db.collection("orders");
+};
+
+const createCars = async ({ make, model, price }) => {
+  const carsCollection = await getCarsCollection();
+  await carsCollection.insertOne({ make, model, price });
+  return {make, model, price}
+};
+
+const createBuyers = async ({ name, address, phone }) => {
+  const buyersCollection = await getBuyersCollection();
+  await buyersCollection.insertOne({ name, address, phone });
+  return {name, address, phone}
+};
+
+const createOrders = async ({ date, carID, buyerID }) => {
+  const carsCollection = await getOrdersCollection();
+  await carsCollection.insertOne({ date, carID, buyerID });
+  return { date, carID, buyerID };
+};
+
+const readOrder = async () => {
+    const userCollection = await getCarsCollection(); 
+    const ret = await userCollection.findOne({"id": ObjectId("615603a34f617c6ae41f559f")});
+    return ret;
+}
+
+const run = async () => {
+  const client = await createClient();
+  const orders = await readOrder();
+  console.log(orders)
+  const newCar = await createCars({
+    make: faker.vehicle.model(),
+    model: faker.vehicle.type(),
+    price: faker.datatype.number(),
+  }).then()
+  const newBuyer = await createBuyers({
+    name: `${faker.name.firstName()} ${faker.name.lastName}`,
+    address: faker.address.streetAddress(),
+    phone: faker.phone.phoneNumber(),
+  });
+  await createOrders({
+    date: faker.date.month(),
+    carID: newCar._id,
+    buyerID: newBuyer._id
+  });
+  await client.close();
+};
+
+run().then();
+
+
+// new ObjectId("615603a34f617c6ae41f559f") carID
+// new ObjectId("615603a34f617c6ae41f55a0") buyerID
+// console.log(createdACar.insertedId)
+//   console.log(createdABuyer.insertedId)
+
+// "carID" : "615603a34f617c6ae41f559f", "buyerID" : "615603a34f617c6ae41f55a0" }  This is was what was returned from mongo
+
